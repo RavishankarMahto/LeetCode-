36a4832443cd4cde23f5c61e78b87535d16b9074
Ravishankar Mahto <ravishankarkumar326@yahoo.com>
2024-12-03 20:00:03 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (52.03%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, new StringBuilder(), 0, 0, n);
+        return result;
+    }
+
+    private void backtrack(List<String> result, StringBuilder current, int open, int close, int max) {
+        // If the current string is valid and complete
+        if (current.length() == max * 2) {
+            result.add(current.toString());
+            return;
+        }
+
+        // If we can add an open parenthesis
+        if (open < max) {
+            current.append("(");
+            backtrack(result, current, open + 1, close, max);
+            current.deleteCharAt(current.length() - 1); // backtrack
+        }
+
+        // If we can add a close parenthesis
+        if (close < open) {
+            current.append(")");
+            backtrack(result, current, open, close + 1, max);
+            current.deleteCharAt(current.length() - 1); // backtrack
+        }
+    }
+}
