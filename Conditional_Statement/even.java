159bc0a25e002b58d2aaf82be5ee9dd1b053532f
Ravishankar Kumar <ravishankarkumar326@yahoo.com>
2024-12-13 22:32:32 +0530

    first commit

---
+++ Conditional_Statement/even.java
@@ -0,0 +1,16 @@
+package Conditional_Statement;
+
+import java.util.*;
+
+public class even {
+    public static void main(String[] args) {
+        
+    int num = 20;
+    if(num % 2 == 0) {
+        System.out.println(num + " is even number");
+    }
+    else {
+        System.out.println(num + " is odd number");
+    }
+}
+}
